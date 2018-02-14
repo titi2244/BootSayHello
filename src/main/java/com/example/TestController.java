9ef2516a4026e510efc077217c011eb8e7b287f5
user1 <user1@gitlab>
2018-02-14 13:49:22 +0900

    ..

---
+++ src/main/java/com/example/TestController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	private static Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@RequestMapping("/sayHello")
+	public String sayHello() {
+		return ("Hi!!! This is TestController sayHello");
+	}
+
+}
