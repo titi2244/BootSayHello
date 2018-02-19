c9729c0365da5024bc78ac3570b1a780f205be87
user1 <user1@gitlab>
2018-02-19 14:13:35 +0900

    ..

---
+++ src/main/java/com/example/HelloWildFlyController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWildFlyController {
+
+
+    @RequestMapping("hello")
+    public String hello(){
+        return ("Hello, SpringBoot on Wildfly");
+    }
+    
+    @RequestMapping("sayHello")
+    public String sayHello(){
+        return ("Hello, SpringBoot on Wildfly for titi2244");
+    }
+}
